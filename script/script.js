0bd37e673426d5299ae4388651975e3ea6ed3fe5
BrendoAires <brendoc.aires@gmail.com>
2021-09-15 23:40:18 -0300

    d

---
+++ script/script.js
@@ -0,0 +1,13 @@
+/*---(Bill + (bill * tip)) / numero pessoas = total */
+/* --(bill * tip) / número de pessoas = tip amount*/
+
+const calculaTotal = function(){
+			const preco = document.getElementById("preco").value;
+			const npessoas = document.getElementById("in-npeople").value;
+			const botao = document.getElementById("bt5").value;
+			const botao = document.getElementById("bt5").value;
+			console.log(botao)
+			
+
+}
+

135d86238d155f4e4fdcd750cd33a645e1b10fe6
BrendoAires <brendoc.aires@gmail.com>
2021-09-19 15:48:32 -0300

    aa

--- script/script.js
+++ script/script.js
@@ -15,25 +15,31 @@
 
 function taxa(tip){
 		
-			let preco = document.getElementById("preco").value;
-			let qtdPessoas = document.getElementById("in-npeople").value;
-			console.log(qtdPessoas);
-			let percent = preco * tip;
-			let total = (preco + percent) / qtdPessoas;
+			let preco = parseFloat(document.getElementById("preco").value);
+			let qtdPessoas = parseInt(document.getElementById("in-npeople").value);
+			let pPessoa = (preco * tip) / qtdPessoas;
+			var total = (preco + (preco * tip)) / qtdPessoas;
+			tPessoa = pPessoa.toFixed(2);
+			tTotal = total.toFixed(2);
+			var totalPessoaFormatado = tPessoa.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+			var totalFormatado = tTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+			
 			console.log(total);
 			
-				const dados = `
-				<div class="resultado">
-							
-					<label>${total}</label>
-				
-				
-							
-				</div>
-		`;
+			
+			const dadosPessoa = `
+							<label>${totalPessoaFormatado}</label>
+				`;
 		
-		const resultado = document.getElementById("label-resultado");
-		resultado.innerHTML = dados;				
+		const resultadoPessoa = document.getElementById("label-resultado-pessoa");
+		resultadoPessoa.innerHTML = dadosPessoa;			
+			
+				const dadosTotal = `
+							<label>${totalFormatado}</label>
+				`;
+		
+		const resultadoTotal = document.getElementById("label-resultado-total");
+		resultadoTotal.innerHTML = dadosTotal;			
 			
 				
 			/*let divEl= document.querySelector('container-resultado');

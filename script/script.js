851afec919b6c4b0b0388a517e9444da4d6b8924
BrendoAires <brendoc.aires@gmail.com>
2021-09-20 23:18:36 -0300

    h

--- script/script.js
+++ script/script.js
@@ -2,17 +2,6 @@
 /* --(bill * tip) / número de pessoas = tip amount*/
 
 
-function pegaDados(){
-			preco = parseFloat(document.getElementById("preco").value);
-			pegaTaxa();
-			qtdPessoas = parseInt(document.getElementById("in-npeople").value);
-			customTip = parseFloat(document.getElementById("input-custom").value);
-		
-		
-			
-}
-
-
 
 /*function customTip(){
 			
@@ -22,28 +11,29 @@ function pegaDados(){
 						imprimeResultado();
 			
 }*/
-
-
-function pegaTaxa(tip){
-				taxa = tip / 100;
+function pegaDados(tip){
+			preco = parseFloat(document.getElementById("preco").value);
+			qtdPessoas = parseInt(document.getElementById("in-npeople").value);
+			customTip = parseFloat(document.getElementById("input-custom").value);
+			taxa = parseFloat(tip / 100);
 }
-
 function calcular(){
 			pegaDados();
+			
 			pPessoa = (preco * taxa) / qtdPessoas;
 			total = (preco + (preco * taxa)) / qtdPessoas;
 			tPessoa = pPessoa.toFixed(2);
 			tTotal = total.toFixed(2);
-			imprimeResultado()
+		
+			console.log(tPessoa, tTotal)
 			
-
 }
 			
 
 			
 			
-function imprimeResultado()	{
-	
+function imprimirResultado()	{
+			calcular();
 			const dadosPessoa = `
 							<label class="label-resultado-pessoa">$${tPessoa}</label>
 				`;
